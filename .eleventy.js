7ad6858e4bdc576b83d90de6d09c04b497296b49
Wiesław Mazurak <wmazurak@gamil.com>
2023-03-11 21:33:15 +0100

    init

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets/');
+  eleventyConfig.addPassthroughCopy('./src/blog/');
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public', // '_site' is by default
+    },
+  };
+};
